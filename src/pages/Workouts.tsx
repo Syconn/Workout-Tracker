910ccde74ee26e707c789274451dae30958b8bd7
Syconn <49461127+Syconn@users.noreply.github.com>
2025-05-22 02:59:21 -0400

    Latest Changes

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { useArrayState, useSetProp } from "../utils/Util";
-import { EditableDropDown } from "../utils/Components";
+import { EditableDropDown, WorkoutsList } from "../utils/Components";
 
-type Reps = {
+export type Reps = {
     weight: number;
     reps: number;
-    superset: Reps;
+    superset: Reps | null;
 }
 
-type Workout = {
+export type Workout = {
     name: string;
     reps: Reps[];
 }
 
-type LiftSession = {
+export type LiftSession = {
     date: string;
     program: string;
     workouts: Workout[];
@@ -34,9 +34,9 @@ export function WorkoutTrackerMenu({ workoutData, modifyWorkoutData }: WorkoutDa
         const stored = localStorage.getItem("liftSession");
         return stored ? JSON.parse(stored) : { date: "", program: "", workouts: [] };
     });
-    const setLift = useSetProp(setLiftSession);
     const [active, setActive] = useState(liftSession.workouts.length != 0);
-    
+    const setLift = useSetProp(setLiftSession);
+
     useEffect(() => {
         localStorage.setItem("liftSession", JSON.stringify(liftSession));
     }, [liftSession]);
@@ -69,20 +69,20 @@ function SetupWorkoutTracker({ setup } : { setup: (date: string, program: string
 }
 
 function WorkoutTracker({ workoutData, modifyWorkoutData }: WorkoutDataProp) {
+    const testWorkout: Workout[] = [{"name": "benchpress", "reps": [{"reps": 6, "weight": 125, "superset": null}]}]
+
     const [workouts, addWorkout, removeWorkout] = useArrayState<Workout>(() => { // MAKE SURE TO CLEAR WORKOUT WHEN ADDING TO MEM
         const stored = localStorage.getItem("workouts");
         return stored ? JSON.parse(stored) as Workout[] : [];
     });
-
-    useEffect(() => {
-        localStorage.setItem("workouts", JSON.stringify(workouts));
-    }, [workouts]);        
+    useEffect(() => localStorage.setItem("workouts", JSON.stringify(workouts)), [workouts]);        
 
     return (
         <>
         <EditableDropDown workouts={workoutData.lastWorkoutOfType.map(v => v.name)}/>
         <div></div>
         Session Exercises:
+        <WorkoutsList workouts={testWorkout} />
         </>
     );
 }
--- src/utils/Components.tsx
+++ src/utils/Components.tsx
@@ -1,10 +1,35 @@
-export function EditableDropDown({ workouts } : { workouts: string[] }) {
+import { Reps, Workout } from "../pages/Workouts";
+
+export function EditableDropDown({ workouts }: { workouts: string[] }) {
     return (
         <>
         <input type="text" list="workoutNames" />
         <datalist id="workoutNames">
-            {workouts.map((val, ind) => (<option key={ind} value={val}/>))}
+            {workouts.map((val, ind) => (<option key={ind} value={val} />))}
         </datalist>
         </>
     );
+}
+
+export function WorkoutsList({ workouts }: { workouts: Workout[] }) {
+    return (
+        <>
+        {workouts.map(v => (<WorkoutList workout={v} />))}
+        </>
+    );
+}
+
+function WorkoutList({ workout }: { workout: Workout }) {
+    return (
+        <dl>
+            <dt>{workout.name}</dt>
+            {workout.reps.map(v => (<dd>{generateRep(v)}</dd>))}
+        </dl>
+    );
+}
+
+function generateRep(rep: Reps): string {
+    let output: string = rep.weight + ": " + rep.reps;
+    if (rep.superset != null) output += " | " +  generateRep(rep.superset);
+    return output;
 }
