2cf9a19e23b8a7cb16e52b1fdf5dd467c690d646
Syconn <49461127+Syconn@users.noreply.github.com>
2025-11-06 13:37:22 -0500

    Removed Old stuff

--- src/utils/Util.tsx
+++ src/utils/Util.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 export function useDebounce<T>(value: T, delay: number): T {
     const [debouncedValue, setDebouncedValue] = useState(value);
@@ -9,68 +9,4 @@ export function useDebounce<T>(value: T, delay: number): T {
     }, [value, delay]);
 
     return debouncedValue;
-}
-
-
-// TODO WILL BE REWORKED
-function useSetProp<T>(setState: React.Dispatch<React.SetStateAction<T>>): <K extends keyof T>(key: K, value: T[K]) => void {
-    return useCallback<<K extends keyof T>(key: K, value: T[K]) => void>(
-      (key, value) => { setState(prev => ({...prev, [key]: value}));}, [setState]
-    );
-}
-
-export function useTypeState<T>(val: T | (() => T)): [T, <K extends keyof T>(key: K, value: T[K]) => void] {
-    const [state, setState] = useState(val);
-    return [state, useSetProp(setState)];
-}
-
-export function useAddArrayState<T>(val: T[] | (() => T[])): [T[], (add: T) => void, (remove: T) => void] {
-    const [get, set] = useState(val);
-
-    const add = (add: T) => {
-        if (add && !get.includes(add)) set([...get, add]);
-    };
-
-    const remove = (remove: T) => {
-        set(get.filter(w => w !== remove));
-    };
-
-    return [get, add, remove];
-}
-
-export function useDynamicState<T>(val: T[] | (() => T[])): [T[], (i: number, v: T) => void, (v: T[]) => void, (i: T | T[]) => void, (amount: number) => void]{
-    const [inputs, setInputs] = useState(val);
-
-    const inputChange = (index: number, value: T) => {
-        const newInputs = [...inputs];
-        newInputs[index] = value;
-        setInputs(newInputs);
-    };
-
-    const changeAll = (value: T[]) => {
-        setInputs(value);
-    };
-    
-    const addInput = (input: T | T[]) => {
-        const itemsToAdd = Array.isArray(input) ? input : [input];
-        setInputs((prev) => [...prev, ...itemsToAdd]);
-    };
-
-    const removeInput = (amount: number) => {
-        setInputs((prev) => prev.slice(0, Math.max(0, prev.length - amount)));
-    }
-
-    return [inputs, inputChange, changeAll, addInput, removeInput];
-}
-
-export function capitalize(s: string): string {
-    let r: string = "";
-    for (let i = 0; i < s.length; i++) {
-        if (i == 0) r += s[0].toLocaleUpperCase();
-        else {
-            if (s[i - 1] == " ") r += s[i].toLocaleUpperCase();
-            else r += s[i].toLocaleLowerCase();
-        }
-    }
-    return r;
 }
