9e75295d1de89adf388593d379072b1e103b88e6
Syconn <49461127+Syconn@users.noreply.github.com>
2026-05-26 14:02:24 -0400

    Test Fix

--- client/src/network/userRequests.ts
+++ client/src/network/userRequests.ts
@@ -43,9 +43,9 @@ export async function loadLastLift(exerciseId: string) {
         body: JSON.stringify({ exerciseId }),
     })
 
-    const body = await res.json();
-    if (!res.ok) console.log(body.error);
-    return body as WorkoutSet[];
+    const text = await res.text();
+    if (!text) return undefined;
+    return JSON.parse(text) as WorkoutSet[];
 }
 
 export async function loadPr(exerciseId: string) {
@@ -57,6 +57,12 @@ export async function loadPr(exerciseId: string) {
     })
 
     const body = await res.json();
-    if (!res.ok) console.log(body.error);
-    return body as WorkoutSet;
+    if (!res.ok) {
+        console.log(body.error);
+        return undefined
+    }
+
+    const text = await res.text();
+    if (!text) return undefined;
+    return JSON.parse(text) as WorkoutSet;
 }
